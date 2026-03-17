2c96964dbb3bebfdbf9703053ba15a1a0e3d37fe
Zhou <xzhou473@cos-4a11216.cos.gatech.edu>
2026-03-17 17:57:19 -0400

    ready publish version

--- NL model/Stattest.m
+++ NL model/Stattest.m
@@ -1,7 +1,9 @@
 %%
 %%%%
-%%%%this is a monte carlo simulation for adding error bar to Fig.3, with
-%%%%given uncertainty to MLD, Dust and SST
+%%%%this script include: 
+%%%%	1.monte carlo simulation for adding error bar to Fig.3, with
+%%%%	given uncertainty to MLD, Dust and SST
+%%%%    2. ACF and QQ plot shown in Fig.S11
 %%%%running this program require the input of NLregression model
 
 %%
@@ -45,6 +47,76 @@ end
 
 end
 
+
+%%
+for i=1:1000
+res_MC(i,:)=obs_webdig-yp_MC(i,:)';
+end
+
+res_mean = mean(res_MC,1);
+res_std  = std(res_MC,0,1);
+res_min  = min(res_MC,[],1);
+res_max  = max(res_MC,[],1);
+n = length(res_mean);
+res_sorted = sort(res_mean);
+p = ((1:n)-0.5)/n;
+q_theory = norminv(p, mean(res_mean), std(res_mean));
+
+%%%std
+if true
+res_upper_std = res_mean + res_std;
+res_lower_std = res_mean - res_std;
+res_upper_std_sorted = sort(res_upper_std);
+res_lower_std_sorted = sort(res_lower_std);
+figure(11)
+clf
+hold on
+
+fill([q_theory fliplr(q_theory)], ...
+     [res_lower_std_sorted fliplr(res_upper_std_sorted)], ...
+     [0.8 0.8 0.8], ...
+     'EdgeColor','none', ...
+     'FaceAlpha',0.5);
+% 1:1 reference line
+plot(q_theory, q_theory, 'r-', 'LineWidth',1.5)
+plot(q_theory, res_sorted, 'kx', 'MarkerSize',6, 'MarkerFaceColor','b')
+
+
+
+
+set(gca,'FontSize',13)
+xlabel('Standard Normal Quantiles')
+ylabel('Quality of Input Sample')
+
+% title('QQ Plot with Monte Carlo Uncertainty')
+
+hold off
+else
+%%%min-max
+figure(12)
+clf
+hold on
+res_upper_mm_sorted = sort(res_max);
+res_lower_mm_sorted = sort(res_min);
+fill([q_theory fliplr(q_theory)], ...
+     [res_lower_mm_sorted fliplr(res_upper_mm_sorted)], ...
+     [0.8 0.8 0.8], ...
+     'EdgeColor','none', ...
+     'FaceAlpha',0.5);
+plot(q_theory, res_sorted, 'bx', 'MarkerSize',4, 'MarkerFaceColor','k')
+plot(q_theory, q_theory, 'r-', 'LineWidth',1.5)
+
+set(gca,'FontSize',13)
+xlabel('Standard Normal Quantiles')
+ylabel('Quality of Input Sample')
+%title('QQ Plot with Monte Carlo Uncertainty (min-max)')
+title('')
+hold off
+
+end
+
+%outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_QQplot_MCtest.png'];print([outfile],'-dpng','-r300');
+%outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_QQplot_b6constant.png'];print([outfile],'-dpng','-r300');
 
 
 %%
@@ -120,13 +192,46 @@ figure(11)
 clf
 autocorr(res,'NumLags',24)
 ylabel('ACF')
+xlabel('Lag (months)')
+set(gca,'fontsize',13)
+title('')    
+%outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_ACF.png'];print([outfile],'-dpng','-r300');
+
+%%
+figure(12)
+autocorr(obs_webdig,'NumLags',24)
+ylabel('ACF')
 xlabel('Lag')
 set(gca,'fontsize',13)
 xlabel('Month')
-%outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_ACF_exp.png'];print([outfile],'-dpng','-r300');
+%outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_ACF_observation.png'];print([outfile],'-dpng','-r300');
+
+%% =========================
+%  Ljung–Box Q-test (custmoize range)
+%  =========================
+n = numel(res);
+
+% sample autocorrelations up to lag 12
+maxLag = 24;
+acf = autocorr(res,'NumLags',maxLag);   % returns lag 0..12
+rho = acf(2:end);                       % lags 1..12
+
+% choose lag range to test
+lags = [4:1:16];
+
+% Ljung–Box-type Q over selected lags only
+Q = n*(n+2) * sum( (rho(lags).^2) ./ (n - lags)' );
+df = numel(lags);
+p = 1 - chi2cdf(Q, df);
+
+fprintf('Custom LBQ over lags %d–%d: Q=%.3f, df=%d, p=%.4g\n', ...
+        lags(1), lags(end), Q, df, p);
+
+
+
 %%
 %%%%
-[pval,bp] = archtest(res), %%%%bp test show heteroscedasticity, residual change influenced by dependent variable changes
+ [h,pValue,stat,cValue] = archtest(res), %%%%bp test show heteroscedasticity, residual change influenced by dependent variable changes
 
 %res_index=(obs_webdig>5);
 %figure(12)
@@ -136,13 +241,31 @@ xlabel('Month')
 %xlabel('Fitted values')
 %ylabel('Residuals')
 %yline(0,'k--')
+
+%%%Spread–Location plot
+res_std=res./std(res);
+spread=sqrt(abs(res_std));
+figure(23)
+clf
+scatter(yp1, spread, 25, 'filled');hold on
+p = polyfit(yp1, spread, 1);
+plot(yp1, polyval(p,yp1),'LineWidth',2)
+xlabel('Fitted values')
+ylabel('Sqrt(|Standardized residuals|)')
+title('Scale-Location Plot')
+grid on
+set(gca,'fontsize',13)
+
+outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_Scale-Location.png'];print([outfile],'-dpng','-r300');
+
+
 %%
 %%%%whether this is normal distribution
 qqplot(res)
 set(gca,'fontsize',13)
 title('B')
 %outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_QQplot.png'];print([outfile],'-dpng','-r300');
-outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_QQplot_b6constant.png'];print([outfile],'-dpng','-r300');
+%outfile=['/media/xzhou473/Seagate Backup Plus Drive/Sargassum/Sarg_for_Xing/Sarg_for_Xing/paperfigure/figure_nonlinearmodel/Fig3_QQplot_b6constant.png'];print([outfile],'-dpng','-r300');
 sk=skewness(res) %more large positive residuals than normal distribution
 ku=kurtosis(res) %underpredicts extreme event, residuals become more postiive during extreme event.
 
